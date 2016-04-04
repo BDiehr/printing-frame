cd7f5364ad7a6da857b1732f26f0e169e0a24a93
Brian Diehr <diehr@Brians-MacBook-Pro.local>
2016-04-03 20:46:33 -0400

    Added d3 demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import PrintFrame from './PrintFrame';
+import { transition } from './d3Visualization';
 
 export default class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.copySVG = this.copySVG.bind(this);
+    this.setSuccessMessage = this.setSuccessMessage.bind(this);
+    this.state = { message: 'Ctrl+P to print!' };
+  }
+
+  copySVG() {
+    const svg = document.getElementById('chart').cloneNode(true);
+    const printContainer = document.getElementById('print-frame-1').contentWindow.document.getElementById('print-container-test');
+    printContainer.appendChild(svg);
+  }
+
+  setSuccessMessage() {
+    const printContainer = document.getElementById('print-frame-1').contentWindow.document.getElementById('print-container-test')
+    while (printContainer.firstChild) printContainer.removeChild(printContainer.firstChild);
+    this.setState({ message: 'Success!' });
+  }
+
   render() {
     return (
       <div>
         <h1>Frame Example</h1>
-        <PrintFrame>
-          <h2>Inside of the iframe</h2>
+        <h3>{this.state.message}</h3>
+        <button onClick={transition}>Transition</button>
+        <PrintFrame
+          beforePrint={this.copySVG}
+          afterPrint={this.setSuccessMessage}
+          >
+          <h1>Printing the SVG</h1>
+          <div id="print-container-test" />
         </PrintFrame>
       </div>
     );
--- src/PrintFrame.js
+++ src/PrintFrame.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Frame from 'react-frame-component';
+import uuid from 'cuid';
 
 const P_KEY_DOWN = 80;
 
 class PrintFrame extends React.Component {
   constructor(props, context) {
     super(props, context);
+    this.frameId = `print-frame-1`;
     this.handlePrint = this.handlePrint.bind(this);
     this.triggerPrint = this.triggerPrint.bind(this);
   }
@@ -21,7 +23,6 @@ class PrintFrame extends React.Component {
   }
 
   handlePrint(event) {
-    console.log('handeling event');
     if((event.ctrlKey || event.metaKey) && event.keyCode == P_KEY_DOWN){
       this.triggerPrint();
       event.preventDefault ? event.preventDefault() : (event.returnValue = false);
@@ -29,7 +30,10 @@ class PrintFrame extends React.Component {
   }
 
   triggerPrint() {
-    const frame = document.getElementById('print-frame').contentWindow;
+    if (this.props.beforePrint != null) {
+      this.props.beforePrint();
+    }
+    const frame = document.getElementById(this.frameId).contentWindow;
     frame.focus();
     frame.print();
     if (this.props.afterPrint != null) {
@@ -39,9 +43,11 @@ class PrintFrame extends React.Component {
 
   render() {
     return (
-      <Frame id="print-frame">
-        {this.props.children}
-      </Frame>
+      <div style={{ visibility: 'hidden' }}>
+        <Frame id={this.frameId}>
+          {this.props.children}
+        </Frame>
+      </div>
     )
   }
 }
